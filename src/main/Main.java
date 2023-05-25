5600ac1fa764882b3c7880ca7326922fdc720b6a
Rafael  Ioshi Imamura Pereira <rafa.ioshi@gmail.com>
2023-05-25 19:03:35 -0300

    Explicação da pilha

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		new Util().start();
+
+		// # Sprint2Codigos
+		// A pilha será usada, para fazer uma trilha das telas acessadas na aplicação
+
+		// Assim quando entrarmos em uma tela, ela era adicionar a pilha
+		
+		// Conforme voltamos para a tela passada a tela do topo será retirada
+
+		// EX:
+		// HOME > DESCONTOS > ALIMENTOS > MOLHOS > EMBALAGEM > 
+
 	}
 
 }
